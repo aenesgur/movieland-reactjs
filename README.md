fc4007a658e2241505cfd46ce69c969848a42b1c
Ahmet_Enes_gur <ahmet_enes_gur@epam.com>
2023-12-14 14:33:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# MovieLand
+
+It is a movieland application that developed with React.js. OMDb API was used for endpoint.
+
+### Setup
+* npm install
+
+* npm run dev
---
+++ src/pages/Home.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react"
+import MovieCard from "../components/movieCard"
+const API_URL = 'https://www.omdbapi.com/?apikey=c032e2d7&'
+import SearchIcon from '../assets/search.svg'
+
+const Home = () => {
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('batman')
+    }, [])
+
+
+    return (
+        <>
+            <div className="app">
+                <h1>MovieLand</h1>
+
+                <div className="search">
+                    <input
+                        placeholder="Search for movies"
+                        value={searchTerm}
+                        onChange={(e) => {setSearchTerm(e.target.value)}}
+                    />
+
+                    <img
+                        src={SearchIcon}
+                        alt="search"
+                        onClick={() => { searchMovies(searchTerm)}}
+                    />
+                </div>
+                {
+                    movies?.length > 0
+                        ? (
+                            <div className="container">
+                                {movies.map((movie) => (<MovieCard movie={movie} />))}
+                            </div>
+                        ) :
+                        (<div className="empty">
+                            <h2>No movies found</h2>
+                        </div>)
+
+                }
+
+            </div>
+        </>
+    );
+}
+
+export default Home
